7b1225d686515e1e5b0506c690eb6b0c9ec9c6dd
Nikhil Shaw <69413813+nikhilshaw3@users.noreply.github.com>
2022-09-11 19:25:20 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -20,7 +20,25 @@ navigator.mediaDevices
 .then((stream)=>{
     mystream = stream
     addVideoStream(myvideo,stream)
+    socket.on("user-connected",(userId)=>{
+        connectToNewUser(userId,stream)
+    })
+    peer.on("call",(call)=>{
+        call.answer(stream)
+        const video = document.createElement("video")
+        call.on("stream",(userVideoStream)=>{
+            addVideoStream(video,userVideoStream)
+        })
+    })
 })
+
+function connectToServer(userId,stream){
+    const call = peer.call(userId,stream)
+    const video = document.createElement("video")
+    call.on("stream",(userVideoStream)=>{
+        audioVideoStream(video,userVideoStream)
+    })
+}
 
 function addVideoStream(video,stream){
     video.srcObject = stream;
